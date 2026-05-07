7f7bddd2fa53770e5e02400ca0373c2859a06b81
milxooo <juliancf245@gmail.com>
2026-05-07 14:40:03 -0500

    fix: corregir importacion de DashboardLayout y añadir props obligatorias

--- src/pages/US07PriorizacionAcademica.tsx
+++ src/pages/US07PriorizacionAcademica.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { DashboardLayout } from '../layouts/DashboardLayout';
+import DashboardLayout from '../layouts/DashboardLayout';
 import { AcademicPriority } from '../features/academic-priority';
 
 const US07PriorizacionAcademica: React.FC = () => {
   return (
-    <DashboardLayout>
+    <DashboardLayout 
+      activeSection="priority" 
+      onNavigate={(id) => console.log('Navigating to:', id)} 
+      onLogout={() => console.log('Logout')}
+    >
       <AcademicPriority />
     </DashboardLayout>
   );
